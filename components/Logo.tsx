21b84a612ee47dba1f26975f6c4dc7c3c93beb2e
Temurbek <temurbekUnical@gmail.com>
2024-06-06 15:11:14 +0500

    ok

---
+++ components/Logo.tsx
@@ -0,0 +1,25 @@
+import { DollarSign } from "lucide-react";
+import React from "react";
+
+function Logo() {
+  return (
+    <a href="/" className="flex items-center gap-2">
+      <DollarSign className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]" />
+      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        Xaznam
+      </p>
+    </a>
+  );
+}
+
+export function LogoMobile() {
+  return (
+    <a href="/" className="flex items-center gap-2">
+      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        Xaznam
+      </p>
+    </a>
+  );
+}
+
+export default Logo;
